b6e98363a7da80595e373795dc2694eba186bb6f
Nguyen Mach <nguyen.mach@seldatinc.com>
2019-05-10 10:36:17 +0700

    Implement #55

--- DMSDriver/DMSDriver/App/Models/GroupLocatonModel/GroupLocatonModel.swift
+++ DMSDriver/DMSDriver/App/Models/GroupLocatonModel/GroupLocatonModel.swift
@@ -13,7 +13,26 @@ class GroupLocatonModel: BaseModel {
     var orders:[Order]?
     
     func getDeliverPackages() -> [Order.Detail] {
-        let result:[Order.Detail] = []
+        var result:[Order.Detail] = []
+        orders?.forEach({ (order) in
+            if order.to?.address == address?.address {
+                let package = order.details ?? []
+                result.append(package)
+            }
+        })
         
+        return result
+    }
+    
+    func getPickupPackages() -> [Order.Detail] {
+        var result:[Order.Detail] = []
+        orders?.forEach({ (order) in
+            if order.from?.address == address?.address {
+                let package = order.details ?? []
+                result.append(package)
+            }
+        })
+        
+        return result
     }
 }
