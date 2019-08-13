cd4f8c8c282321fa5ec137e5ace40a721a9f95ab
Trung Vo <trung.vo@seldatinc.com>
2019-08-13 16:46:25 +0700

    localized strings

--- DMSDriver/DMSDriver/App/ViewControllers/Order/LoadUnloadOrderVC/LoadUnloadOrderVC.swift
+++ DMSDriver/DMSDriver/App/ViewControllers/Order/LoadUnloadOrderVC/LoadUnloadOrderVC.swift
@@ -129,13 +129,15 @@ extension LoadUnloadOrderVC {
                         return
                     }
                 }
-                showAlertView("Loaded cartons must be less than or equal \(detail.cartonsInPallet ?? 0)")
+                let message = String(format: "loaded-cartons-must-be-less-than-or-equal".localized, "\(detail.cartonsInPallet ?? 0)")
+                showAlertView(message)
                 return
             }
             updateLoadedQuantity(detail:detail)
             // call without loaded carton
         } else {
-            showAlertView("Loaded quantity must be less than or equal \(detail.qty ?? 0)")
+            let message = String(format: "loaded-quantity-must-be-less-than-or-equal".localized, "\(detail.qty ?? 0)")
+            showAlertView(message)
             return
         }
     }
