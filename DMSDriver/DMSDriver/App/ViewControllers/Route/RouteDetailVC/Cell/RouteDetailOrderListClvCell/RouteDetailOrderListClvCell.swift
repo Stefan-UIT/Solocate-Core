ce260c9ab56cd76807dc9fee8b5f29c92d3e9240
Trung Vo <trung.vo@seldatinc.com>
2019-08-29 14:26:30 +0700

    AS-827 [IOS][Route detail] - The Order information is hidden after user focus on "WMS Code" search text box

--- DMSDriver/DMSDriver/App/ViewControllers/Route/RouteDetailVC/Cell/RouteDetailOrderListClvCell/RouteDetailOrderListClvCell.swift
+++ DMSDriver/DMSDriver/App/ViewControllers/Route/RouteDetailVC/Cell/RouteDetailOrderListClvCell/RouteDetailOrderListClvCell.swift
@@ -9,8 +9,13 @@
 import UIKit
 import SVProgressHUD
 
+protocol RouteDetailOrderListClvCellDelegate: class {
+    func didBeginEditSearchText()
+    func didEndEditSearchText()
+}
 
 class RouteDetailOrderListClvCell: UICollectionViewCell {
+    weak var delegate: RouteDetailOrderListClvCellDelegate?
     
     @IBOutlet weak var tbvContent: UITableView?
     @IBOutlet weak var noOrdersLabel: UILabel?
@@ -233,7 +238,7 @@ extension RouteDetailOrderListClvCell{
 
 extension RouteDetailOrderListClvCell:BaseSearchViewDelegate{
     func tfSearchShouldBeginEditing(view: BaseSearchView, textField: UITextField) {
-        //
+        self.delegate?.didBeginEditSearchText()
     }
     
     func tfSearchShouldChangeCharactersInRangeWithString(view: BaseSearchView, text: String) {
@@ -242,7 +247,7 @@ extension RouteDetailOrderListClvCell:BaseSearchViewDelegate{
     }
     
     func tfSearchDidEndEditing(view: BaseSearchView, textField: UITextField) {
-        //
+        self.delegate?.didEndEditSearchText()
     }
     
     func doSearch(strSearch:String? = nil)  {
