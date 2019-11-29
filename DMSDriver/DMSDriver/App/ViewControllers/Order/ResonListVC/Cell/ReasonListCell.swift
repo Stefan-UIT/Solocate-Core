d5e8906722836370dfcc149f06ac2eaba2da7021
Trung Vo <trung.vo@seldatinc.com>
2019-11-29 17:28:02 +0700

    [Refactor] Reason List

--- DMSDriver/DMSDriver/App/ViewControllers/Order/ResonListVC/Cell/ReasonListCell.swift
+++ DMSDriver/DMSDriver/App/ViewControllers/Order/ResonListVC/Cell/ReasonListCell.swift
@@ -17,13 +17,14 @@ class ReasonListCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
-
+    
+    func configureCell(reason:Reason) {
+        lblTitle?.text =  reason.name
+        lblSubtitle?.text = ""
+    }
 }
