a9e2b8bdba1fe30520e862c60e6976b247ab5c33
Phong Nguyen Tang <phong.nguyen@seldatinc.com>
2019-12-16 14:48:37 +0700

    [PurchaseOrder] Add PurchaseOrder Storyboard & ViewController

---
+++ DMSDriver/DMSDriver/App/ViewControllers/PurchaseOrder/PurchaseOrderDetailVC/Cell/PurchaseOrderPicktupTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PurchaseOrderPicktupTableViewCell.swift
+//  DMSDriver
+//
+//  Created by Phong Nguyen on 12/16/19.
+//  Copyright © 2019 machnguyen_uit. All rights reserved.
+//
+
+import UIKit
+
+class PurchaseOrderPicktupTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
