581e2523675056b8ca95c9162c66826daa007d4f
Phong Nguyen Tang <phong.nguyen@seldatinc.com>
2020-01-15 11:41:03 +0700

    fixed params API updateRentingOrderDetailStatus

--- DMSDriver/DMSDriver/App/Managers/Services/APIServices/API+Order.swift
+++ DMSDriver/DMSDriver/App/Managers/Services/APIServices/API+Order.swift
@@ -22,7 +22,7 @@ extension BaseAPIService{
     @discardableResult
     func updateRentingOrderStatus(nextStatus:Int, rentingOrderDetailId:Int, message:String?, callback: @escaping APICallback<ResponseDataModel<RentingOrder>>) -> APIRequest? {
         let path = String(format:PATH_REQUEST_URL.UPDATE_RENTING_ORDER_DETAIL_STATUS.URL, "\(rentingOrderDetailId)")
-        let params:[String:Any] = ["renting_order_status_id": nextStatus, "msg": message]
+        let params:[String:Any] = ["renting_order_detail_status_id": nextStatus, "msg": message]
         if ReachabilityManager.isNetworkAvailable {
             return request(method: .PUT,
                            path: path,
